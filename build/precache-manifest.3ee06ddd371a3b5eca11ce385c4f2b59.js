b91e9fdccda9ef910c1a41342342895339bda13e
Timothy Claassens <tim.claassens@gmail.com>
2019-04-30 14:31:49 +1200

    Added build output so I don't have to build on a raspberry pi in order to save a massive amount of time.

---
+++ build/precache-manifest.3ee06ddd371a3b5eca11ce385c4f2b59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9dffaba5166d6dabb08",
+    "url": "/static/css/main.d6dc93a1.chunk.css"
+  },
+  {
+    "revision": "e9dffaba5166d6dabb08",
+    "url": "/static/js/main.f75f4b53.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "565e810f4e6eeb869673",
+    "url": "/static/js/2.f4f97cd2.chunk.js"
+  },
+  {
+    "revision": "05b56ce7f54286fddad52e113efa29c4",
+    "url": "/static/media/brand-logo.05b56ce7.png"
+  },
+  {
+    "revision": "5c697348948debf492be8e1b8e5aca81",
+    "url": "/index.html"
+  }
+];
